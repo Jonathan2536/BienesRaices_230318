a5e797414a2ef570d48299f6984333852f7a84b8
Jonathan <jonathanesponja333@gmail.com>
2024-12-13 14:57:50 -0600

    Practica14
    Finalizando practica14

--- Config/db.js
+++ Config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config({path:'.env'})
 const db = new Sequelize(process.env.BD_NAME,process.env.BD_USER, process.env.BD_PASS, {
     host: process.env.BD_HOST,
-    port: 3306,
+    port: 3307,
     dialect: 'mysql',
     define: {
         timestamps: true
